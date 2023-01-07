bedb959012e97441a179cbddf8eb189499b97f8e
Martin Langbecker <Martin.Langbecker@gmail.com>
2023-01-07 19:37:22 +0100

    fix test, 0.1.3

--- test.js
+++ test.js
@@ -1,38 +1,41 @@
-import * as isRoughlyEqual from 'is-roughly-equal';
+import isRoughlyEqual from 'is-roughly-equal';
 import { readStations as stations } from 'mav-stations';
 import moment from 'moment-timezone';
-import tape from 'tape';
+import test from 'tape';
 import { queryPrices as prices } from './lib/index.js';
 
 const hour = 60 * 60 * 1000;
 
-const validDate = (date) => {
+const isValidDate = (date) => {
   return isRoughlyEqual(36 * hour, +when, +new Date(date));
 };
 
-const findStation = (id) => {
-  new Promise((resolve, reject) =>
-    stations()
-      .on('error', reject)
-      .on('data', (station) => {
-        if (station.id === id) resolve;
-      })
-      .on('end', reject)
-  );
+const findStation = async (id) => {
+  for await (const station of stations()) {
+    if (station.id === id) return true;
+  }
+
+  return false;
 };
 
-const validLeg = async (test, leg) => {
+const isValidLeg = async (test, leg) => {
   test.ok(leg, 'missing leg');
 
-  test.ok(validDate(leg.departure), 'invalid departure date');
+  test.ok(isValidDate(leg.departure), 'invalid departure date');
   test.ok(leg.origin, 'missing `origin`');
-  test.ok(await findStation(leg.origin.id), 'station not found');
+  test.ok(
+    await findStation(leg.origin.id),
+    `station ${leg.origin.id} not found`
+  );
   if (leg.departureDelay) test.equal(typeof leg.departureDelay, 'number');
   if (leg.departurePlatform) test.equal(typeof leg.departurePlatform, 'string');
 
-  test.ok(validDate(leg.arrival), 'invalid arrival date');
+  test.ok(isValidDate(leg.arrival), 'invalid arrival date');
   test.ok(leg.destination, 'missing `destination`');
-  test.ok(await findStation(leg.destination.id), 'station not found');
+  test.ok(
+    await findStation(leg.destination.id),
+    `station ${leg.destination.id} not found`
+  );
   if (leg.departureDelay) test.equal(typeof leg.departureDelay, 'number');
   if (leg.arrivalPlatform) test.equal(typeof leg.arrivalPlatform, 'string');
 
@@ -41,7 +44,7 @@ const validLeg = async (test, leg) => {
   test.equal(typeof leg.line.mode, 'string');
 };
 
-const validPrice = (test, price) => {
+const isValidPrice = (test, price) => {
   test.ok(price, 'missing price');
 
   test.equal(price.currency, 'EUR');
@@ -50,16 +53,16 @@ const validPrice = (test, price) => {
   if (price.name) test.equal(typeof price.name, 'string');
 };
 
-const validJourney = async (test, journey) => {
+const isValidJourney = async (test, journey) => {
   test.ok(journey, 'missing journey');
 
   test.ok(Array.isArray(journey.legs), 'missing legs');
   test.ok(journey.legs.length > 0, 'missing legs');
   for (const leg of journey.legs) {
-    await validLeg(test, leg);
+    await isValidLeg(test, leg);
   }
 
-  validPrice(test, journey.price);
+  isValidPrice(test, journey.price);
 };
 
 const hamburgHbf = '008001071';
@@ -74,12 +77,12 @@ const when = moment
   .day(1 + 7)
   .toDate();
 
-tape('Hamburg Hbf -> Hegyeshalom Hbf', async (test) => {
+test('Hamburg Hbf -> Hegyeshalom Hbf', async (test) => {
   const results = await prices(hamburgHbf, hegyeshalom, when);
   test.ok(Array.isArray(results));
   test.ok(results.length > 0, 'no results');
   for (const result of results) {
-    await validJourney(test, result);
+    await isValidJourney(test, result);
   }
 
   test.end();
